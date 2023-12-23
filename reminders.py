a13aab17406c9dec828c19799842f789ef4ffa17
IntellScale <aisolutionsua@gmail.com>
2023-12-23 14:57:49 +0200

    changed reminders.py to send the link to payment each month

--- reminders.py
+++ reminders.py
@@ -43,7 +43,11 @@ def send_monday_reminders():
 
     # Send the message to all users
     for chat_id in chat_ids:
-        send_message(chat_id, "Доброго ранку!🌞 \nНагадування про тижневий звіт \n\nhttps://docs.google.com/forms/d/1ThR6kOxHfd8Fwt92CiFnXA3nWb7SdLixt8hSFsNUqzw/edit?pli=1")
+        try:
+            send_message(chat_id, "Доброго ранку!🌞 \nНагадування про тижневий звіт \n\nhttps://docs.google.com/forms/d/1ThR6kOxHfd8Fwt92CiFnXA3nWb7SdLixt8hSFsNUqzw/edit?pli=1")
+        except Exception as e:
+            print(e)
+
 
 def send_first_of_month_reminders():
     # Get the current date in Kyiv timezone
@@ -52,20 +56,23 @@ def send_first_of_month_reminders():
     # Check if today is the first day of the month
     if today.day == 1:
         # Get a list of all chat IDs (user IDs)
-        chat_ids = ["579467950"]  # Replace with your actual user IDs
+        chat_ids = get_all_ids()
 
         # Send the message to all users
         for chat_id in chat_ids:
-            send_message(chat_id, "Sending first day of the month reminders...")
+            try:
+                send_message(chat_id, "Початок місяця! 🌞 \nНагадування про оплату \n\nhttp://secure.wayforpay.com/donate/d550158201eff")
+            except Exception as e:
+                print(e)
 
 if __name__ == '__main__':
-    print("REMINDERS -- WORKING")
+    print("REMINDERS --- WORKING")
     # # Schedule Monday reminders every Monday at 10 AM Kyiv time
-    # schedule.every().monday.at("10:00").do(send_monday_reminders)
+    schedule.every().monday.at("10:00").do(send_monday_reminders)
 
     # Schedule first day of the month reminders at 10 AM Kyiv time
-    schedule.every(10).seconds.do(send_monday_reminders).tag("first_of_month")
+    schedule.every().day.at("10:00").do(send_first_of_month_reminders).tag("first_of_month")
 
     while True:
         schedule.run_pending()
-        time.sleep(1)  # Sleep for 1 minute to balance responsiveness and efficiency
+        time.sleep(60)  # Sleep for 1 minute to balance responsiveness and efficiency
